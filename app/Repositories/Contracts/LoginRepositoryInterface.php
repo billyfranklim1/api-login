e0bf7c78ebd985a5cebd3dfcf8dcf55e7b03c634
Billy <billyfranklim@gmail.com>
2024-03-15 14:06:33 -0300

    ✨ feat(repository): add register method to LoginRepositoryInterface

--- app/Repositories/Contracts/LoginRepositoryInterface.php
+++ app/Repositories/Contracts/LoginRepositoryInterface.php
@@ -5,4 +5,8 @@ namespace App\Repositories\Contracts;
 interface LoginRepositoryInterface
 {
     public function login(array $credentials);
+    public function register(array $data);
+    public function logout();
+    public function refresh();
+    public function me();
 }
--- app/Repositories/Eloquent/LoginRepository.php
+++ app/Repositories/Eloquent/LoginRepository.php
@@ -5,17 +5,47 @@ namespace App\Repositories\Eloquent;
 use App\Models\User;
 use App\Repositories\Contracts\LoginRepositoryInterface;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 
 class LoginRepository implements LoginRepositoryInterface
 {
     public function login(array $credentials)
     {
-        $user = User::where('email', $credentials['email'])->first();
+        $user = User::where('username', $credentials['username'])->first();
         if (!$user || !Hash::check($credentials['password'], $user->password)) {
-            throw new \Exception('The provided credentials are incorrect.');
+            return null;
         }
 
-        return $user->createToken('token')->plainTextToken;
+        return $user;
     }
+
+    public function register(array $data)
+    {
+        $user = User::create([
+            'name' => $data['name'],
+            'username' => $data['username'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+        ]);
+
+        return $user;
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+    }
+
+    public function refresh()
+    {
+        $userId = Auth::id();
+        return User::find($userId);
+    }
+
+    public function me()
+    {
+        return Auth::user();
+    }
+
 
 }
